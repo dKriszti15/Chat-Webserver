734203109240277ca34e11dad1484202cefaef8f
daczk <kisdacz@yahoo.com>
2024-07-28 21:54:14 +0300

    1_login + logout

--- backend/db/users.db.js
+++ backend/db/users.db.js
@@ -25,6 +25,30 @@ export class User{
             return -1;
         }
     }
+
+    async getUserPassword(username){
+        const query = 'SELECT pwd FROM users WHERE username = ?';
+        try{
+            const pass = await pool.query(query,[username]);
+            return pass[0][0].pwd;
+        }
+        catch (e){
+            console.log(e);
+            return -1;
+        }
+    }
+
+    async getUserRole(username){
+        const query = 'SELECT role FROM users WHERE username = ?';
+        try{
+            const role = await pool.query(query,[username]);
+            return role;
+        }
+        catch (e){
+            console.log(e);
+            return -1;
+        }
+    }
 }
 
 const Users = new User();
--- backend/routers/default.js
+++ backend/routers/default.js
@@ -10,17 +10,45 @@ const def = express.Router();
 //def.use(express.static(staticDir));
 
 def.get('/', (req, res) => {
-    res.status(200).render('index.ejs');
+    res.status(200).render('index.ejs', {loggedUser: ''});
 })
 
 def.get('/login', (req,res) => {
-    res.status(200).render('login.ejs', { message: ''});
+    res.status(200).render('login.ejs', { message: '', loggedUser: ''});
 })
 
+def.post('/login', express.json(), async (req, res) => {
+    console.log(req.body);
+    const correctPw = await Users.getUserPassword(req.body.username);
+    if(correctPw === null){
+        res.status(404).render('login.ejs', {message: `The user '${req.body.username}' does not exist!`, loggedUser: ''})
+    }else{
+        const iscorrect = await bcrypt.compare(req.body.password, correctPw);
+        if (iscorrect){
+            req.session.username = req.body.username;
+            const role = await Users.getUserRole(req.body.username);
+            req.session.role = role;
+            res.status(200).render('index.ejs', {message: `Welcome, ${req.body.username}!`,loggedUser: req.session.username});
+        }else{
+            res.status(405).render('login.ejs', {message: 'Wrong password! Try again!', loggedUser: ''});
+        }
+        
+    }
+    res.status(200).end();
+})
 
+def.get('/logout', (req, res) => {
+    req.session.destroy(async (e) => {
+        if(e){
+            res.status(500).send('Logout error.');
+        }else {
+            res.status(200).render('index.ejs', {message: 'Logout succesful!', loggedUser: ''});
+        }
+    })
+})
 
 def.get('/register', (req,res) =>  {
-    res.status(200).render('register.ejs', { message: ''});
+    res.status(200).render('register.ejs', { message: '', loggedUser: ''});
 })
 
 def.post('/register', async (req, res) => {
