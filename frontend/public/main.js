9024b69bd4002344ebc5a63cfdb0e23cd06286f1
daczk <kisdacz@yahoo.com>
2024-08-04 22:29:49 +0300

    4_few bug fixed + notification sounds

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,5 +1,3 @@
-
-
 const socket = io();
 
 socket.on('clients-total', (data) => {
@@ -10,6 +8,9 @@ const messageForm = document.getElementById('messageSendingForm');
 const messageInput = document.getElementById('messageInput');
 const loggedUser = document.getElementById('loggedUserP');
 const messages = document.getElementById('messageContainer');
+
+const messageSentSound = new Audio('/sentSound.mp3');
+const messageRcvdSound = new Audio('/rcvdSound.mp3');
 
 messageForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -24,15 +25,18 @@ function sendMessage(){
         message: messageInput.value,
         dateTime: new Date()
     }
+    
     let datePart = msg.dateTime.toString().split('T')[0];
     datePart = datePart.replace(/ /g,'/');
     datePart = datePart.split(':')[0] + ':' + datePart.split(':')[1];
     datePart = datePart.split('/')[1] + '/' + datePart.split('/')[2] + '/' + datePart.split('/')[3] + " | " + datePart.split('/')[4];
-    console.log(datePart);
 
     msg.dateTime = datePart;
 
     socket.emit('message', msg);
+
+    messageInput.value = '';
+
     addMessageToUI(true, msg);
     
 }
@@ -46,11 +50,17 @@ function addMessageToUI(ownMessage, msg) {
     const newMessage = `
         <li class="${ownMessage ? 'messageRight' : 'messageLeft'}">
             <p class="message">
-              ${msg.from}: 
-              <span>${msg.message} ● ${(msg.dateTime)}</span>
+              <span>${msg.from}: ${msg.message}</span>
+              <br>
+              <span> ● ${(msg.dateTime)}</span>
             </p>
           </li>
           `
-  
+    if(ownMessage){
+        messageSentSound.play();
+    }
+    else{
+        messageRcvdSound.play();
+    }
     messages.innerHTML += newMessage;
   }
